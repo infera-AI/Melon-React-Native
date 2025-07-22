483b7c0b7e4686b0e8981fd41519c817ce9fe7e4
jhy <775822253@qq.com>
2025-07-22 15:22:27 +0800

    集成zustand状态管理及包含自动持久化功能

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
-// 状态管理统一导出
+// 状态管理统一导出，组件中就可以import { useUserStore } from '@/stores';   不导出时只能完整路径import { useUserStore } from '../stores/modules/user.store';
+export * from './modules/user.store';
 export * from './authSlice';
 export * from './userSlice';
